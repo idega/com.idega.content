e40478cdf402c7564b7d02344aff126f3b1ebfc3
Grimur Jonsson <gimmi@idega.com>
2004-12-15 14:30:56 +0000

    works !!! (version 0.0023)

--- src/java/com/idega/content/presentation/WebDAVFileDetails.java
+++ src/java/com/idega/content/presentation/WebDAVFileDetails.java
@@ -5,7 +5,9 @@ package com.idega.content.presentation;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
+import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.Vector;
 
 import javax.faces.component.UIComponent;
 import javax.faces.component.html.HtmlOutputLink;
@@ -28,45 +30,43 @@ import com.idega.webface.WFUtil;
  */
 public class WebDAVFileDetails extends IWBaseComponent {
 
-	private HtmlPanelGrid grid = new HtmlPanelGrid();
-	private boolean fileFound = false;
 	WebdavResource resource = null;
 	
 	protected void initializeContent() {
 		this.setId(this.getId());
 
+		HtmlPanelGrid grid = new HtmlPanelGrid();
 		grid.setId(this.getId()+"_grid");
 		grid.setBorder(2);
-		grid.setColumns(4);
-		
-		if (fileFound) {
-			grid.getChildren().add(WFUtil.getText(resource.getName()));
-			
-			HtmlOutputLink link = new HtmlOutputLink();
-			link.setValue(resource.getHttpURL());
-			link.getChildren().add(WFUtil.getText("Get file"));
-			grid.getChildren().add(link);
-			
-			//boolean doesExist = getChildren().contains(grid);
-			for (Iterator iter = getChildren().iterator(); iter.hasNext();) {
-				UIComponent comp2 = (UIComponent) iter.next();
-				System.out.println("Parent = "+comp2.toString());
-			}
-			this.add(grid);
-			
-			System.out.println("ADDING FILE !!!!!!!");
-		}
-//		boolean vbPath = false;
-//		vbPath = (webDavPath.indexOf("#{") > -1);
-//		
-//		if (vbPath) {
-//			HtmlOutputText text = WFUtil.getTextVB(webDavPath);
-//			table.getChildren().add(text);
-//		} else {
+		grid.setColumns(2);
+		grid.getFacets().put("header", WFUtil.getText("Document Details"));
 
-		//addColumn(text);
-		//this.getChildren().add(table);
-//		this.add(text);
+		if (resource != null) {
+			grid.getChildren().add(WFUtil.getText(resource.getName()));
+			HtmlOutputLink link = new HtmlOutputLink();
+			link.setValue(resource.getPath());
+			link.getChildren().add(WFUtil.getText("Download/View"));
+			grid.getChildren().add(link);
+			try {
+				Enumeration enumer = resource.propfindMethod("version-history");
+				while (enumer.hasMoreElements()) {
+					Object el = enumer.nextElement();
+					grid.getChildren().add(WFUtil.getText("version"));
+//					WebdavResource r = new WebdavResource();
+					grid.getChildren().add(WFUtil.getText(el.toString()));
+					//System.out.println("Enumer = "+enumer.nextElement());
+				}
+ 			} catch (HttpException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			this.getChildren().add(grid);
+		} 
+		
+		
+		
+		
 	}
 	
 	public void encodeBegin(FacesContext context) throws IOException {
@@ -74,14 +74,24 @@ public class WebDAVFileDetails extends IWBaseComponent {
 		String path = (String) WFUtil.invoke(webDavPath);
 		try {
 			IWSlideSession ss = (IWSlideSession) IBOLookup.getSessionInstance(IWContext.getInstance(), IWSlideSession.class);
-			resource = ss.getWebdavResource(path);
-			fileFound = !resource.isCollection();
-			if (fileFound) {
-				initializeContent();
-			}
-//			if (getParent().getChildren().contains(this)) {
-//				getParent().getChildren().remove(this);
-//			}
+			WebdavResource oldRes = resource;
+			WebdavResource newRes = ss.getWebdavResource(path);
+			if (!newRes.isCollection() && (oldRes == null || oldRes.getName().equals(newRes.getName())) ) {
+				resource = newRes;
+				
+				this.setInitialized(false);
+				Vector gr = new Vector();
+				for (Iterator iter = getChildren().iterator(); iter.hasNext();) {
+					UIComponent element = (UIComponent) iter.next();
+					if (element instanceof HtmlPanelGrid) {
+						gr.add((HtmlPanelGrid) element);
+					}
+				}
+				if (gr != null) {
+					getChildren().removeAll(gr);
+				}
+			} 
+			
 		} catch (IBOLookupException e) {
 			e.printStackTrace();
 		} catch (UnavailableIWContext e) {
@@ -95,11 +105,7 @@ public class WebDAVFileDetails extends IWBaseComponent {
 		}
 		super.encodeBegin(context);
 	}
-	
-	public void encodeEnd(FacesContext context) throws IOException{
-		getChildren().remove(grid);
-	}
-	
+		
 	public void encodeChildren(FacesContext context) throws IOException{
 		super.encodeChildren(context);
 		for (Iterator iter = getChildren().iterator(); iter.hasNext();) {
