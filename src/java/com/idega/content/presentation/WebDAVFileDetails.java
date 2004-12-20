1d39c8a001a508602d0b9548406d2111dd5aa66b
Eirikur Hrafnsson <eiki@idega.com>
2004-12-20 03:07:45 +0000

    rewrite of all version stuff

--- src/java/com/idega/content/presentation/WebDAVFileDetails.java
+++ src/java/com/idega/content/presentation/WebDAVFileDetails.java
@@ -3,16 +3,14 @@
  */
 package com.idega.content.presentation;
 
-import java.util.Enumeration;
-
+import java.util.Iterator;
+import java.util.List;
 import javax.faces.component.html.HtmlOutputLink;
-
-import org.apache.webdav.lib.BaseProperty;
-import org.apache.webdav.lib.ResponseEntity;
 import org.apache.webdav.lib.WebdavResource;
-
 import com.idega.presentation.Table;
+import com.idega.presentation.text.DownloadLink;
 import com.idega.slide.util.VersionHelper;
+import com.idega.slide.util.WebdavResourceVersion;
 import com.idega.util.Timer;
 import com.idega.webface.WFUtil;
 
@@ -23,77 +21,86 @@ public class WebDAVFileDetails extends ContentBlock {
 
 	protected void initializeContent() {
 		this.setId(this.getId());
-
 		WebdavResource resource = getWebdavResource();
-		
 		Table table = new Table();
-		table.setId(this.getId()+"_table");
-		table.setBorder(1);
-		table.setBorderColor("Blue");
+		table.setId(this.getId() + "_table");
+		table.setWidth("100%");
 		int row = 1;
 		table.mergeCells(1, row, 2, row);
 		table.add(WFUtil.getText("Document details"));
-		
 		if (resource != null) {
-			String resourceName = resource.getName(); 
-
+			String resourceName = resource.getName();
 			HtmlOutputLink link = new HtmlOutputLink();
 			link.setValue(resource.getPath());
-			link.setId(getId()+"_dl");
-			link.getChildren().add(WFUtil.getText("Download/View"));
-
+			link.setId(getId() + "_dl");
+			link.getChildren().add(WFUtil.getText("Download"));
 			++row;
 			table.add(WFUtil.getText(resourceName), 1, row);
 			table.add(link, 2, row);
-			
-			
 			Timer timer = new Timer();
 			timer.start();
 			
-			Table vTable = new Table();
-			vTable.setId(table.getId()+"_ver");
-			vTable.setBorder(1);
-			vTable.setBorderColor("Red");
+			
+			List versions = VersionHelper.getAllVersions(resource);
+			Table vTable = new Table(8,versions.size()+1);
+			vTable.setId(table.getId() + "_ver");
+			//setListStyleClass("wf_list");
+			vTable.setRowStyleClass(1,"wf_listheading");
+			vTable.setStyleClass("wf_listtable");
+			//vTable.setHeaderClass("wf_listheading");
+			
 			int vRow = 1;
+			int vColumn = 1;
+			vTable.add("Version", vColumn, vRow);
+			vTable.add("Download", ++vColumn, vRow);
+			vTable.add("User", ++vColumn, vRow);
+			vTable.add("Comment", ++vColumn, vRow);
+			vTable.add("Checkout", ++vColumn, vRow);
+			vTable.add("Checkin", ++vColumn, vRow);
+			vTable.add("Last modified", ++vColumn, vRow);
+
 			
-			vTable.add("Version", 2, vRow);
-			vTable.add("User", 3, vRow);
-			vTable.add("Comment", 4, vRow);
-			
-			Enumeration enumer = VersionHelper.getAllVersions(resource);
-			while (enumer.hasMoreElements()) {
-				ResponseEntity element = (ResponseEntity) enumer.nextElement();
-				Enumeration props = element.getProperties();
-				++vRow;
-				vTable.add(resourceName, 1, vRow);
-				while (props.hasMoreElements()) {
-					BaseProperty prop = (BaseProperty) props.nextElement();
-					String propName = prop.getLocalName();
-					int colToAdd = 1;
-					if (propName.equals(VersionHelper.PROPERTY_VERSION_NAME)) { // Version number
-						colToAdd = 2;
-						vTable.setAlignment(colToAdd, vRow, Table.HORIZONTAL_ALIGN_CENTER);
-					} else if (propName.equals(VersionHelper.PROPERTY_CREATOR_DISPLAYNAME)) { 
-						colToAdd = 3;
-					} else if (propName.equals(VersionHelper.PROPERTY_VERSION_COMMENT)) {
-						colToAdd = 4;
+			if (!versions.isEmpty()) {
+				Iterator iter = versions.iterator();
+				while (iter.hasNext()) {
+					vColumn = 0;
+					WebdavResourceVersion version = (WebdavResourceVersion) iter.next();
+					++vRow;
+				
+					if(vRow%2==0){
+						vTable.setRowStyleClass(vRow,"wf_listevenrow");
 					}
-					vTable.add(prop.getPropertyAsString(), colToAdd, vRow);
+					else{
+						vTable.setRowStyleClass(vRow,"wf_listoddrow");
+					}
+					
+					
+					//vTable.add(resourceName, 1, vRow);
+					String versionName = version.getVersionName();
+					vTable.add(versionName, ++vColumn, vRow);
+					DownloadLink versionPath = new DownloadLink("Download/View");
+					versionPath.setId("dl_"+vRow);
+					String url = version.getURL();
+					versionPath.setRelativeFilePath(url);
+					//so we have a sensable name for the file!
+					versionPath.setAlternativeFileName("v"+versionName.replace('.','_')+"-"+resourceName);
+					//versionPath.getChildren().add(WFUtil.getText("Download/View"));
+					vTable.add(versionPath, ++vColumn, vRow);
+					vTable.add(version.getCreatorDisplayName(), ++vColumn, vRow);
+					vTable.add(version.getComment(), ++vColumn, vRow);
+					vTable.add(version.getCheckedOut(), ++vColumn, vRow);
+					vTable.add(version.getCheckedIn(), ++vColumn, vRow);
+					vTable.add(version.getLastModified(), ++vColumn, vRow);
 				}
 			}
 			timer.stop();
 			++vRow;
 			vTable.add("Creation time", 3, vRow);
 			vTable.add(timer.getTimeString(), 4, vRow);
-
 			++row;
 			table.mergeCells(1, row, 2, row);
 			table.add(vTable, 1, row);
-				
 			this.getChildren().add(table);
-			
-		} 
-		
+		}
 	}
-
 }
