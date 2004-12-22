2e119a3d6edc5d344a9374a3841a0d1c94da3c3d
Eirikur Hrafnsson <eiki@idega.com>
2004-12-22 02:18:25 +0000

    added preview and new folder actions

--- src/java/com/idega/content/presentation/ContentViewer.java
+++ src/java/com/idega/content/presentation/ContentViewer.java
@@ -1,13 +1,10 @@
 package com.idega.content.presentation;
 
 import java.io.IOException;
-
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-import javax.faces.el.ValueBinding;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
-
 import com.idega.idegaweb.IWBundle;
 import com.idega.webface.WFBlock;
 import com.idega.webface.WFTitlebar;
@@ -25,49 +22,57 @@ public class ContentViewer extends WFBlock {
 	private static String LIST = "ac_list";
 	private static String FILE_DETAILS = "ac_file_details";
 	private static String PREVIEW = "ac_preview";
+	private static String NEW_FOLDER = "ac_folder";
 	
 	private String currentAction = null;
 	
 	private boolean renderListLink = true;
 	private boolean renderDetailsLink = false;
 	private boolean renderPreviewLink = false;
+	private boolean renderNewFolderLink = true;
 	
-	private Boolean renderWebDAVList;
-	private Boolean renderWebDAVFileDetails;
-	private Boolean renderWebDAVFilePreview;
+	private boolean renderWebDAVList = true;
+	private boolean renderWebDAVFileDetails = false;
+	private boolean renderWebDAVFilePreview = false;
+	private boolean renderWebDAVNewFolder = false;
 	
 	public ContentViewer() {
 		super();
 	}
 	
-	public void initializeContent() {
+	protected IWBundle getBundle() {
+		return ContentBlock.getBundle();
+	}
+	
+	
+	public void initializeContent() {	
 
 		WFTitlebar tb = new WFTitlebar();
 		tb.setValueRefTitle(true);
 		tb.setTitleText("WebDAVListBean.webDAVPath");
 //		tb.getChildren().add(title);
 		this.setTitlebar(tb);
-		renderWebDAVList = new Boolean(true);
-		renderWebDAVFileDetails = new Boolean(false);
 		
 //		setToolbarEmbeddedInTitlebar(false);
 		
 //		setToolbar();
 		setToolbarEmbeddedInTitlebar(false);
-		
-
 		String startFolder = (String) this.getAttributes().get("startFolder");
 		String rootFolder = (String) this.getAttributes().get("rootFolder");
 		
 		WebDAVList list = new WebDAVList();
 		list.setId(getId()+"_list");
-		list.setRendered(renderWebDAVList.booleanValue());
+		list.setRendered(renderWebDAVList);
 		list.setStartFolder(startFolder);
 		list.setRootFolder(rootFolder);
 		
 		WebDAVFileDetails details = new WebDAVFileDetails();
-		details.setRendered(renderWebDAVFileDetails.booleanValue());
+		details.setRendered(renderWebDAVFileDetails);
 		details.setId(getId()+"_details");
+		
+		WebDAVFilePreview preview = new WebDAVFilePreview();
+		preview.setRendered(renderWebDAVFilePreview);
+		preview.setId(getId()+"_preview");
 		
 //		getFacets().put(BAR, bar);
 //		getChildren().add(bar);
@@ -75,34 +80,9 @@ public class ContentViewer extends WFBlock {
 		
 		getFacets().put(LIST, list);
 		getFacets().put(FILE_DETAILS, details);
+		getFacets().put(PREVIEW, preview);
 //		getChildren().add(list);
 //		getChildren().add(details);
-	}
-	
-	protected IWBundle getBundle() {
-		return ContentBlock.getBundle();
-	}
-	
-	public Boolean getRenderWebDAVList() {
-    if (renderWebDAVList != null) return renderWebDAVList;
-    ValueBinding vb = getValueBinding("renderWebDAVList");
-    Boolean v = vb != null ? (Boolean)vb.getValue(getFacesContext()) : null;
-    return v != null ? v : null;
-	}
-	
-	public void setRenderWebDAVList(Boolean render) {
-		renderWebDAVList = render;
-	}
-	
-	public Boolean getRenderWebDAVFileDetails() {
-    if (renderWebDAVFileDetails != null) return renderWebDAVFileDetails;
-    ValueBinding vb = getValueBinding("renderWebDAVFileDetails");
-    Boolean v = vb != null ? (Boolean)vb.getValue(getFacesContext()) : null;
-    return v != null ? v : null;
-	}
-	
-	public void setRenderWebDAVFileDetails(Boolean render) {
-		renderWebDAVFileDetails = render;
 	}
 	
 	public void encodeBegin(FacesContext context) throws IOException {
@@ -126,8 +106,13 @@ public class ContentViewer extends WFBlock {
 				if (currentAction == null) {
 					setRenderFlags(FILE_DETAILS);
 				}
+				else if(PREVIEW.equals(currentAction)){
+					setRenderFlags(PREVIEW);
+				}
 				
 				super.setToolbar(getToolbar());
+				
+				
 			}
 		}
 		super.encodeBegin(context);
@@ -143,14 +128,20 @@ public class ContentViewer extends WFBlock {
 
 		UIComponent list = getFacet(LIST);
 		if (list != null) {
-			list.setRendered(renderWebDAVList.booleanValue());
+			list.setRendered(renderWebDAVList);
 			renderChild(context, list);
 		}
 		
 		UIComponent details = getFacet(FILE_DETAILS);
 		if (details != null) {
-			details.setRendered(renderWebDAVFileDetails.booleanValue());
+			details.setRendered(renderWebDAVFileDetails);
 			renderChild(context, details);
+		}
+		
+		UIComponent preview = getFacet(PREVIEW);
+		if (preview != null) {
+			preview.setRendered(renderWebDAVFilePreview);
+			renderChild(context, preview);
 		}
 		
 		
@@ -159,31 +150,43 @@ public class ContentViewer extends WFBlock {
 	public WFToolbar getToolbar() {
 		WFToolbar bar = new WFToolbar();
 		
-		WFToolbarButton list = new WFToolbarButton("images/list.jpg", getBundle());
+		WFToolbarButton list = new WFToolbarButton("/images/list.jpg",getBundle());
 		list.getAttributes().put(PARAMETER_ACTION, LIST);
 		list.setId(getId()+"_btnList");
 		list.setToolTip("Document List");
 		list.setActionListener(WFUtil.createMethodBinding("#{contentviewerbean.processAction}", new Class[]{ActionEvent.class}));
+//		list.setValueBinding("rendered", WFUtil.createValueBinding("#{contentviewerbean.renderListLink}"));
 		list.setRendered(renderListLink);
 
-		WFToolbarButton details = new WFToolbarButton("images/details.jpg", getBundle());
+		WFToolbarButton details = new WFToolbarButton("/images/details.jpg",getBundle());
 		details.getAttributes().put(PARAMETER_ACTION, FILE_DETAILS);
 		details.setId(getId()+"_btnDetails");
 		details.setToolTip("Document Details");
 		details.setActionListener(WFUtil.createMethodBinding("#{contentviewerbean.processAction}", new Class[]{ActionEvent.class}));
 		details.setRendered(renderDetailsLink);
 
-		WFToolbarButton preview = new WFToolbarButton("images/preview.jpg", getBundle());
+		WFToolbarButton preview = new WFToolbarButton("/images/preview.jpg",getBundle());
 		preview.getAttributes().put(PARAMETER_ACTION, PREVIEW);
 		preview.setId(getId()+"_btnPreview");
 		preview.setToolTip("Document Preview");
 		preview.setActionListener(WFUtil.createMethodBinding("#{contentviewerbean.processAction}", new Class[]{ActionEvent.class}));
 		preview.setRendered(renderPreviewLink);
-
+		
+		WFToolbarButton newFolder = new WFToolbarButton("/images/newfolder.gif",getBundle());
+		newFolder.getAttributes().put(PARAMETER_ACTION, NEW_FOLDER);
+		newFolder.setId(getId()+"_btnNewFolder");
+		newFolder.setToolTip("New Folder");
+		newFolder.setActionListener(WFUtil.createMethodBinding("#{contentviewerbean.processAction}", new Class[]{ActionEvent.class}));
+		newFolder.setRendered(renderNewFolderLink);
+		
+		bar.addButton(newFolder);
 		bar.addButton(list);
 		bar.addButton(details);
 		bar.addButton(preview);
+		
 
+//		super.setToolbar(bar);
+//		getChildren().add(bar);
 		return bar;
 	}
 
@@ -202,40 +205,52 @@ public class ContentViewer extends WFBlock {
 	private void setRenderFlags(String action) {
 		System.out.println("[ContentViewer] action = "+action);
 		if (LIST.equals(action)) {
-			setRenderWebDAVList( new Boolean(true));
-			setRenderWebDAVFileDetails(new Boolean(false));
-			renderWebDAVFilePreview = new Boolean(false);
+			renderWebDAVList = true;
+			renderWebDAVFileDetails = false;
+			renderWebDAVFilePreview = false;
+			renderWebDAVNewFolder = false;
 		} else if (FILE_DETAILS.equals(action)) {
-			setRenderWebDAVList( new Boolean(false));
-			setRenderWebDAVFileDetails(new Boolean(true));
-			renderWebDAVFilePreview = new Boolean(false);
+			renderWebDAVList = false;
+			renderWebDAVFileDetails = true;
+			renderWebDAVFilePreview = false;
+			renderWebDAVNewFolder = false;
 		} else if (PREVIEW.equals(action)) {
-			setRenderWebDAVList( new Boolean(false));
-			setRenderWebDAVFileDetails(new Boolean(false));
-			renderWebDAVFilePreview = new Boolean(true);
+			renderWebDAVList = false;
+			renderWebDAVFileDetails = false;
+			renderWebDAVFilePreview = true;
+			renderWebDAVNewFolder = false;
+		}else if (NEW_FOLDER.equals(action)) {
+			renderWebDAVList = false;
+			renderWebDAVFileDetails = false;
+			renderWebDAVFilePreview = false;
+			renderWebDAVNewFolder = true;
 		}
 	}
 	
 	public Object saveState(FacesContext ctx) {
 		Object values[] = new Object[10];
 		values[0] = super.saveState(ctx);
-		values[1] = renderWebDAVList;
-		values[2] = renderWebDAVFileDetails;
-		values[3] = renderWebDAVFilePreview;
-		values[4] = new Boolean(renderListLink);
-		values[5] = new Boolean(renderDetailsLink);
-		values[6] = new Boolean(renderPreviewLink);
+		values[1] = new Boolean(renderWebDAVList);
+		values[2] = new Boolean(renderWebDAVFileDetails);
+		values[3] = new Boolean(renderWebDAVFilePreview);
+		values[4] = new Boolean(renderWebDAVNewFolder);
+		values[5] = new Boolean(renderListLink);
+		values[6] = new Boolean(renderDetailsLink);
+		values[7] = new Boolean(renderPreviewLink);
+		values[8] = new Boolean(renderNewFolderLink);
 		return values;
 	}
 
 	public void restoreState(FacesContext ctx, Object state) {
 		Object values[] = (Object[]) state;
 		super.restoreState(ctx, values[0]);
-		renderWebDAVList = (Boolean) values[1];
-		renderWebDAVFileDetails = (Boolean) values[2];
-		renderWebDAVFilePreview = (Boolean) values[3];
-		renderListLink = ((Boolean) values[4]).booleanValue();
-		renderDetailsLink = ((Boolean) values[5]).booleanValue();
-		renderPreviewLink = ((Boolean) values[6]).booleanValue();
+		renderWebDAVList = ((Boolean) values[1]).booleanValue();
+		renderWebDAVFileDetails = ((Boolean) values[2]).booleanValue();
+		renderWebDAVFilePreview = ((Boolean) values[3]).booleanValue();
+		renderWebDAVNewFolder = ((Boolean) values[4]).booleanValue();
+		renderListLink = ((Boolean) values[5]).booleanValue();
+		renderDetailsLink = ((Boolean) values[6]).booleanValue();
+		renderPreviewLink = ((Boolean) values[7]).booleanValue();
+		renderPreviewLink = ((Boolean) values[8]).booleanValue();
 	}
 }
