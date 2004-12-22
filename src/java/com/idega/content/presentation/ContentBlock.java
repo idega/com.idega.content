9100f46e289b4b0825503bdef748c0f2bcb0b31f
Eirikur Hrafnsson <eiki@idega.com>
2004-12-22 02:13:59 +0000

    using extended webdavresource

--- src/java/com/idega/content/presentation/ContentBlock.java
+++ src/java/com/idega/content/presentation/ContentBlock.java
@@ -4,13 +4,9 @@ import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.Iterator;
 import java.util.Vector;
-
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-
 import org.apache.commons.httpclient.HttpException;
-import org.apache.webdav.lib.WebdavResource;
-
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
 import com.idega.idegaweb.IWBundle;
@@ -18,6 +14,7 @@ import com.idega.idegaweb.UnavailableIWContext;
 import com.idega.presentation.IWBaseComponent;
 import com.idega.presentation.IWContext;
 import com.idega.slide.business.IWSlideSession;
+import com.idega.slide.util.WebdavExtendedResource;
 import com.idega.webface.WFUtil;
 
 /**
@@ -25,11 +22,12 @@ import com.idega.webface.WFUtil;
  */
 public abstract class ContentBlock extends IWBaseComponent {
 
+	private WebdavExtendedResource resource = null;
+
 	public static final String IW_BUNDLE_IDENTIFIER = "com.idega.content";
-	
-	private WebdavResource resource = null;
+
 	private static IWBundle bundle = null;
-	
+
 	protected abstract void initializeContent();
 
 	public static IWBundle getBundle() {
@@ -38,70 +36,74 @@ public abstract class ContentBlock extends IWBaseComponent {
 		}
 		return bundle;
 	}
-		
+
 	private static void setupBundle() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		IWContext iwContext = IWContext.getIWContext(context);
 		bundle = iwContext.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER);
 	}
-	
-	protected WebdavResource getWebdavResource() {
+
+	protected WebdavExtendedResource getWebdavExtendedResource() {
 		return resource;
 	}
-	
+
 	public void encodeBegin(FacesContext context) throws IOException {
 		String webDavPath = (String) this.getAttributes().get("path");
 		String path = null;
-		
 		if (webDavPath == null) {
-			path = (String) WFUtil.createMethodBinding("#{WebDAVListBean.getClickedFilePath}", null).invoke(context, null);
-		} else {
+			path = (String) WFUtil.createMethodBinding("#{WebDAVListBean.getClickedFilePath}", null).invoke(context,
+					null);
+		}
+		else {
 			path = (String) WFUtil.invoke(webDavPath);
 		}
 		try {
-			IWSlideSession ss = (IWSlideSession) IBOLookup.getSessionInstance(IWContext.getInstance(), IWSlideSession.class);
-			WebdavResource oldRes = resource;
-			WebdavResource newRes = ss.getWebdavResource(path);
-			if (!newRes.isCollection() && (oldRes == null || oldRes.getName().equals(newRes.getName())) ) {
+			IWSlideSession ss = (IWSlideSession) IBOLookup.getSessionInstance(IWContext.getInstance(),
+					IWSlideSession.class);
+			WebdavExtendedResource oldRes = resource;
+			WebdavExtendedResource newRes = ss.getWebdavResource(path);
+			if (!newRes.isCollection() && (oldRes == null || oldRes.getName().equals(newRes.getName()))) {
 				resource = newRes;
-				
 				this.setInitialized(false);
 				Vector gr = new Vector();
 				for (Iterator iter = getChildren().iterator(); iter.hasNext();) {
 					UIComponent element = (UIComponent) iter.next();
 					//if (element instanceof HtmlPanelGrid) {
-						gr.add(element);
+					gr.add(element);
 					//}
 				}
 				if (gr != null) {
 					getChildren().removeAll(gr);
 				}
-			} 
-			
-		} catch (IBOLookupException e) {
+			}
+		}
+		catch (IBOLookupException e) {
 			e.printStackTrace();
-		} catch (UnavailableIWContext e) {
+		}
+		catch (UnavailableIWContext e) {
 			e.printStackTrace();
-		} catch (HttpException e) {
+		}
+		catch (HttpException e) {
 			e.printStackTrace();
-		} catch (RemoteException e) {
+		}
+		catch (RemoteException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 		super.encodeBegin(context);
 	}
-		
-	public void encodeChildren(FacesContext context) throws IOException{
+
+	public void encodeChildren(FacesContext context) throws IOException {
 		super.encodeChildren(context);
 		for (Iterator iter = getChildren().iterator(); iter.hasNext();) {
 			UIComponent child = (UIComponent) iter.next();
-			renderChild(context,child);
+			renderChild(context, child);
 		}
 	}
-		
+
 	public boolean getRendersChildren() {
 		return true;
 	}
-	
 }
