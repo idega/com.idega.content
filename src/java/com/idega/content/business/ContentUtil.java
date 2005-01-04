3409bac530e244d40f92ce50e4481e86b5f57f82
Joakim Johnsson <joakim@idega.com>
2005-01-04 16:55:42 +0000

    Uncomment

--- src/java/com/idega/content/business/ContentUtil.java
+++ src/java/com/idega/content/business/ContentUtil.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ContentUtil.java,v 1.1 2005/01/04 15:23:30 joakim Exp $
+ * $Id: ContentUtil.java,v 1.2 2005/01/04 16:55:42 joakim Exp $
  *
  * Copyright (C) 2004 Idega. All Rights Reserved.
  *
@@ -12,14 +12,13 @@ package com.idega.content.business;
 
 /**
  * 
- * Last modified: $Date: 2005/01/04 15:23:30 $ by $Author: joakim $
+ * Last modified: $Date: 2005/01/04 16:55:42 $ by $Author: joakim $
  *
  * @author Joakim Johnson
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ContentUtil {
 	public static final String CONTENT_PATH = "/files/cms";
-	//Not yet created
-//	public static final String ARTICLE_PATH = "/files/article";
-//	public static final String PAGES_PATH = "/files/pages";
+	public static final String ARTICLE_PATH = "/files/cms/article";
+	public static final String PAGES_PATH = "/files/cms/pages";
 }
