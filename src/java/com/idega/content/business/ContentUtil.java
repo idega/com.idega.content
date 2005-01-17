83264fd94e044a8a3089a322f36745a3aa878607
Gudmundur Agust Saemundsson <gummi@idega.com>
2005-01-17 17:20:18 +0000

    added method to get the bundle and a module prefix constant.

--- src/java/com/idega/content/business/ContentUtil.java
+++ src/java/com/idega/content/business/ContentUtil.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ContentUtil.java,v 1.2 2005/01/04 16:55:42 joakim Exp $
+ * $Id: ContentUtil.java,v 1.3 2005/01/17 17:20:18 gummi Exp $
  *
  * Copyright (C) 2004 Idega. All Rights Reserved.
  *
@@ -9,16 +9,38 @@
  */
 package com.idega.content.business;
 
+import javax.faces.context.FacesContext;
+import com.idega.idegaweb.IWBundle;
+import com.idega.presentation.IWContext;
+
 
 /**
  * 
- * Last modified: $Date: 2005/01/04 16:55:42 $ by $Author: joakim $
+ * Last modified: $Date: 2005/01/17 17:20:18 $ by $Author: gummi $
  *
  * @author Joakim Johnson
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class ContentUtil {
 	public static final String CONTENT_PATH = "/files/cms";
 	public static final String ARTICLE_PATH = "/files/cms/article";
 	public static final String PAGES_PATH = "/files/cms/pages";
+	
+	public static final String MODULE_PREFIX = "cms_";
+	public static final String IW_BUNDLE_IDENTIFIER = "com.idega.content";
+	
+	private static IWBundle bundle = null;
+	
+	public static IWBundle getBundle() {
+		if (bundle == null) {
+			setupBundle();
+		}
+		return bundle;
+	}
+
+	private static void setupBundle() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		IWContext iwContext = IWContext.getIWContext(context);
+		bundle = iwContext.getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER);
+	}
 }
