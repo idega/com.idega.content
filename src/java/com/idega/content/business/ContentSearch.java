a864b6b07067fa856622fa84037b76c55842a14f
Eirikur Hrafnsson <eiki@idega.com>
2005-01-19 01:45:55 +0000

    Improved more OOP design of the com.idega.search framwork or related/implementing classes

--- src/java/com/idega/content/business/ContentSearch.java
+++ src/java/com/idega/content/business/ContentSearch.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ContentSearch.java,v 1.2 2005/01/18 10:43:38 gimmi Exp $
+ * $Id: ContentSearch.java,v 1.3 2005/01/19 01:45:55 eiki Exp $
  * Created on Jan 17, 2005
  *
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -11,11 +11,10 @@ package com.idega.content.business;
 
 import java.io.IOException;
 import java.net.URLDecoder;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.Map;
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HostConfiguration;
@@ -32,76 +31,117 @@ import org.apache.webdav.lib.search.SearchRequest;
 import org.apache.webdav.lib.search.SearchScope;
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
-import com.idega.core.search.business.Searchable;
+import com.idega.core.search.business.Search;
+import com.idega.core.search.business.SearchPlugin;
+import com.idega.core.search.business.SearchQuery;
+import com.idega.core.search.data.BasicSearch;
+import com.idega.core.search.data.BasicSearchResult;
+import com.idega.core.search.data.SimpleSearchQuery;
 import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWMainApplication;
 import com.idega.presentation.IWContext;
-import com.idega.presentation.PresentationObject;
-import com.idega.presentation.text.Link;
+import com.idega.slide.business.IWSlideService;
 import com.idega.slide.business.IWSlideSession;
 
 
 /**
  * 
- *  Last modified: $Date: 2005/01/18 10:43:38 $ by $Author: gimmi $
- * This class implements the Searchable interface and can therefor be used in a Search block for searching contents of the files in webdav.
+ *  Last modified: $Date: 2005/01/19 01:45:55 $ by $Author: eiki $
+ * This class implements the Searchplugin interface and can therefore be used in a Search block for searching contents of the files in the iwfile system.
  * To use it simply register this class as a iw.searchable component in a bundle.
  * @author <a href="mailto:eiki@idega.com">Eirikur S. Hrafnsson</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
-public class ContentSearch implements Searchable {
+public class ContentSearch implements SearchPlugin {
 
 	public static final String SEARCH_NAME_LOCALIZABLE_KEY = "content_search.name";
-	public static final String DEFAULT_LINK_STYLE_CLASS = "content_search_link";
+	public static final String SEARCH_DESCRIPTION_LOCALIZABLE_KEY = "content_search.description";
 	
-	private String linkStyleClass = DEFAULT_LINK_STYLE_CLASS;
-	
-	static final PropertyName DISPLAYNAME = new PropertyName("DAV:", "displayname");
+	public static final String SEARCH_TYPE = "document";
 
+	static final PropertyName DISPLAYNAME = new PropertyName("DAV:", "displayname");
 	static final PropertyName LASTMODIFIED = new PropertyName("DAV:", "getlastmodified");
 	
+	private IWMainApplication iwma = null;
+	private HttpURL httpURL;
+
 	
-	/**
-	 * 
-	 */
 	public ContentSearch() {
 		super();
 	}
 
 	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#initialize(com.idega.presentation.IWContext)
+	 * @see com.idega.core.search.business.SearchPlugin#getAdvancedSearchSupportedParameters()
 	 */
-	public boolean initialize(IWContext iwc) {
-		// TODO Auto-generated method stub
+	public List getAdvancedSearchSupportedParameters() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.search.business.SearchPlugin#getSupportsSimpleSearch()
+	 */
+	public boolean getSupportsSimpleSearch() {
 		return true;
 	}
 
 	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#destroy(com.idega.presentation.IWContext)
+	 * @see com.idega.core.search.business.SearchPlugin#getSupportsAdvancedSearch()
 	 */
-	public void destroy(IWContext iwc) {
-		// TODO Auto-generated method stub
-		
+	public boolean getSupportsAdvancedSearch() {
+		return false;
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.search.business.SearchPlugin#initialize(com.idega.idegaweb.IWMainApplication)
+	 */
+	public boolean initialize(IWMainApplication iwma) {
+		this.iwma = iwma;
+		try {
+			IWSlideService service = (IWSlideService) IBOLookup.getServiceInstance(iwma.getIWApplicationContext(), IWSlideService.class);
+			httpURL = service.getWebdavServerURL();
+		}
+		catch (IBOLookupException e) {
+			e.printStackTrace();
+			return false;
+		}
+		catch (RemoteException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 
 	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#getSimpleSearchResults(java.lang.String, com.idega.presentation.IWContext)
+	 * @see com.idega.core.search.business.SearchPlugin#destroy(com.idega.idegaweb.IWMainApplication)
 	 */
-	public Collection getSimpleSearchResults(String queryString, IWContext iwc) {
+	public void destroy(IWMainApplication iwma) {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.core.search.business.SearchPlugin#createSearch(com.idega.core.search.business.SearchQuery)
+	 */
+	public Search createSearch(SearchQuery searchQuery) {
 		List results = new ArrayList();
+		BasicSearch searcher = new BasicSearch();
+		searcher.setSearchName(getSearchName());
+		searcher.setSearchType(SEARCH_TYPE);
+		searcher.setSearchQuery(searchQuery);
+		
 		try {
-			IWSlideSession session = (IWSlideSession) IBOLookup.getSessionInstance(iwc, IWSlideSession.class);
-			//IWSlideService service =
-			// (IWSlideService)IBOLookup.getServiceInstance(iwuc,IWSlideService.class);
+			IWSlideSession session = (IWSlideSession) IBOLookup.getSessionInstance(IWContext.getInstance(), IWSlideSession.class);
+
 			SearchRequest s = new SearchRequest();
 			s.addSelection(DISPLAYNAME);
 			s.addSelection(LASTMODIFIED);
 			s.addScope(new SearchScope("files"));
-			SearchExpression expression = s.contains(queryString);
+			SearchExpression expression = s.contains(((SimpleSearchQuery)searchQuery).getSimpleSearchQuery());
 			s.setWhereExpression(expression);
 			String search = s.asString();
 			System.out.println(search);
 			SearchMethod method = new SearchMethod("/content", search);
-			HttpURL httpURL = new HttpURL("localhost", 8090, "/content");
+
 			HttpClient client = new HttpClient();
 			client.setState(new WebdavState());
 			HostConfiguration hostConfig = client.getHostConfiguration();
@@ -122,89 +162,51 @@ public class ContentSearch implements Searchable {
 			for (int i = 0; i < headers.length; i++) {
 				System.out.println(headers[i].toString());
 			}
-			Enumeration enumer = method.getAllResponseURLs();
-			while (enumer.hasMoreElements()) {
-				String url = (String) enumer.nextElement();
-				System.out.println(url);
-				results.add(url);
+			Enumeration enum = method.getAllResponseURLs();
+			while (enum.hasMoreElements()) {
+				String fileURI = (String) enum.nextElement();
+				BasicSearchResult result = new BasicSearchResult();
+				
+				result.setSearchResultType(SEARCH_TYPE);
+				result.setSearchResultURI(fileURI);
+				result.setSearchResultName(URLDecoder.decode(fileURI.substring(fileURI.lastIndexOf("/")+1)));
+				result.setSearchResultExtraInformation(URLDecoder.decode(fileURI.substring(0,fileURI.lastIndexOf("/")+1)));
+				
+				results.add(result);
 			}
-			return results;
+			
+			searcher.setSearchResults(results);
 		}
 		catch (IBOLookupException e) {
 			e.printStackTrace();
 		}
 		catch (SearchException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		catch (HttpException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return new ArrayList();
+		
+		return searcher;
 	}
 
 	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#getAdvancedSearchResults(java.util.Map, com.idega.presentation.IWContext)
+	 * @see com.idega.core.search.business.SearchPlugin#getSearchName()
 	 */
-	public Collection getAdvancedSearchResults(Map queryMap, IWContext iwc) {
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#getAdvancedSearchSupportedParameters()
-	 */
-	public List getAdvancedSearchSupportedParameters() {
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#getSupportsSimpleSearch()
-	 */
-	public boolean getSupportsSimpleSearch() {
-		return true;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#getSupportsAdvancedSearch()
-	 */
-	public boolean getSupportsAdvancedSearch() {
-		return false;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#getSearchName(com.idega.presentation.IWContext)
-	 */
-	public String getSearchName(IWContext iwc) {
+	public String getSearchName() {
 		IWBundle bundle = ContentUtil.getBundle();
-		return bundle.getResourceBundle(iwc).getLocalizedString(SEARCH_NAME_LOCALIZABLE_KEY,"Documents");
+		return bundle.getResourceBundle(IWContext.getInstance()).getLocalizedString(SEARCH_NAME_LOCALIZABLE_KEY,"Documents");
 	}
 
 	/* (non-Javadoc)
-	 * @see com.idega.core.search.business.Searchable#getResultPresentation(java.lang.Object, com.idega.presentation.IWContext)
+	 * @see com.idega.core.search.business.SearchPlugin#getSearchDescription()
 	 */
-	public PresentationObject getResultPresentation(Object resultObject, IWContext iwc) {
-		String fileURI = (String)resultObject;
-		Link link = new Link(URLDecoder.decode(fileURI.substring(fileURI.lastIndexOf("/")+1))+" ("+fileURI.substring(0,fileURI.lastIndexOf("/")+1)+")",fileURI);
-		link.setStyleClass(getLinkStyleClass());
-		return link;
-	}
-	
-	/**
-	 * @return Returns the linkStyleClass.
-	 */
-	public String getLinkStyleClass() {
-		return linkStyleClass;
-	}
-	/**
-	 * @param linkStyleClass The linkStyleClass to set.
-	 */
-	public void setLinkStyleClass(String linkStyleClass) {
-		this.linkStyleClass = linkStyleClass;
+	public String getSearchDescription() {
+		IWBundle bundle = ContentUtil.getBundle();
+		return bundle.getResourceBundle(IWContext.getInstance()).getLocalizedString(SEARCH_DESCRIPTION_LOCALIZABLE_KEY,"Searches the contents of documents in an IdegaWeb file system.");
 	}
 	
 }
