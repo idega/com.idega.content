a163883ebe15a87dbb13373ccc3d22b0b5ceb00a
Joakim Johnsson <joakim@idega.com>
2005-01-28 13:53:06 +0000

    Cleanup and comments

--- src/java/com/idega/content/business/MetadataUtil.java
+++ src/java/com/idega/content/business/MetadataUtil.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MetadataUtil.java,v 1.1 2005/01/24 17:15:58 joakim Exp $
+ * $Id: MetadataUtil.java,v 1.2 2005/01/28 13:53:06 joakim Exp $
  *
  * Copyright (C) 2004 Idega. All Rights Reserved.
  *
@@ -15,21 +15,18 @@ import java.util.ArrayList;
 import java.util.Properties;
 import java.util.StringTokenizer;
 
-
 /**
  * 
- * Last modified: $Date: 2005/01/24 17:15:58 $ by $Author: joakim $
+ * Last modified: $Date: 2005/01/28 13:53:06 $ by $Author: joakim $
  * 
  * Utility class for Metadata
  *
  * @author Joakim Johnson
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class MetadataUtil {
-//	public static final String[] metadataType = new String[] {"categories","keywords","publisher"};
 	private static ArrayList metadataType = new ArrayList();
 	private static final String TYPE_KEY = "type";
-	
 	private static final String METADATA_PATH = "/properties/MetadataType.properties";
 	
 	/**
@@ -40,8 +37,10 @@ public class MetadataUtil {
 		//Only read file first time
 		if(metadataType.size() == 0) {
 			Properties prop = new Properties();
+			//Path to properties file
 			String path = new ContentUtil().getBundle().getBundleBaseRealPath()+METADATA_PATH;
 			try {
+				//read property and store comma separated values into arraylist
 				prop.load(new java.io.FileInputStream(path));
 				String type = prop.getProperty(TYPE_KEY);
 				StringTokenizer st = new StringTokenizer(type,",");
