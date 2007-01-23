9d099e712068f3acdcc2295ced9670d11595788d
Gediminas Paulauskas <gediminas@idega.com>
2007-01-23 15:30:54 +0000

    rewrite to store categories in properties file rather than comma-separated string

--- src/java/com/idega/content/business/CategoryBean.java
+++ src/java/com/idega/content/business/CategoryBean.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CategoryBean.java,v 1.4 2007/01/23 10:25:53 valdas Exp $
+ * $Id: CategoryBean.java,v 1.5 2007/01/23 15:30:54 gediminas Exp $
  *
  * Copyright (C) 2004 Idega. All Rights Reserved.
  *
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
+
 import org.apache.commons.httpclient.HttpException;
 import org.apache.webdav.lib.util.WebdavStatus;
 import com.idega.business.IBOLookup;
@@ -25,6 +27,7 @@ import com.idega.presentation.IWContext;
 import com.idega.slide.business.IWSlideService;
 import com.idega.slide.business.IWSlideSession;
 import com.idega.slide.util.WebdavRootResource;
+import com.idega.util.StringHandler;
 
 
 /**
@@ -32,27 +35,58 @@ import com.idega.slide.util.WebdavRootResource;
  * Class for manipulating Categories that are stored in slide.<br/>
  * Includes functions for getting and setting all the available categories
  * </p>
- *  Last modified: $Date: 2007/01/23 10:25:53 $ by $Author: valdas $
+ *  Last modified: $Date: 2007/01/23 15:30:54 $ by $Author: gediminas $
  * 
  * @author <a href="mailto:Joakim@idega.com">Joakim</a>,<a href="mailto:tryggvi@idega.com">Tryggvi Larusson</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class CategoryBean {
 	
 	private static String BEAN_KEY="ContentCategoryBean";
 	
-	//private static final String CATEORY_FIX_PREFIX = "/agura-intra/content";
-	private static final String CATEORY_CONFIG_PATH = "/files/cms/";
-	private static final String CATEORY_CONFIG_FILE = CATEORY_CONFIG_PATH+"categories.prop";
-//	private static final String CATEORY_CONFIG_FILE = CATEORY_CONFIG_PATH+"cat.prp";
+	private static final String CATEGORY_CONFIG_PATH = "/files/cms/";
+	/**
+	 * @deprecated this file will no longer be used
+	 */
+	private static final String CATEGORY_CONFIG_FILE = CATEGORY_CONFIG_PATH+"categories.prop";
+	private static final String CATEGORY_PROPERTIES_FILE = CATEGORY_CONFIG_PATH+"categories.strings";
 	private IWMainApplication iwma;
+	private WebDAVResourceBundle resourceBundle;
 	
 	public static final String CATEGORY_DELIMETER = ",";
 
 	protected CategoryBean(IWMainApplication iwma){
 		this.iwma=iwma;
+		this.resourceBundle = new WebDAVResourceBundle(CATEGORY_PROPERTIES_FILE);
+		migrateOldFile();
 	}
 	
+	private void migrateOldFile() {
+		if (getCategories().isEmpty()) {
+			Collection cats = getCategoriesFromString(getCategoriesAsString());
+			TreeMap map = new TreeMap();
+			for (Iterator iter = cats.iterator(); iter.hasNext();) {
+				String cat = (String) iter.next();
+				// TODO: should generate keys for each old category, but in that case
+				// need to change categories on all existing articles.
+				// Leave it as is for now to not break them
+				//String key = getCategoryKey(cat);
+				String key = cat;
+				map.put(key, cat);
+			}
+			resourceBundle.putAll(map);
+
+			/*
+			try {
+				WebdavResource resource = getSlideService().getWebdavResourceAuthenticatedAsRoot(CATEGORY_CONFIG_FILE);
+				resource.deleteMethod();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			*/
+		}
+	}
+
 	/**
 	 * <p>
 	 * Get the instance of the bean for this application.
@@ -82,14 +116,19 @@ public class CategoryBean {
 	 * <p> Get a collection of categories </p>
 	 * @return collection of strings
 	 */
-	public Collection<String> getCategories() {
-		Collection<String> ret = getCategoriesFromString(getCategoriesAsString());
-		return ret;
+	public Collection getCategories() {
+		//Collection ret = getCategoriesFromString(getCategoriesAsString());
+		return resourceBundle.keySet();
+	}
+	
+	public String getCategoryName(String categoryKey) {
+		return resourceBundle.getString(categoryKey);
 	}
 	
 	/**
 	 * <p>Getts all the categories as one String (comma separated)</p>
 	 * @return categories
+	 * @deprecated this file is no longer used
 	 */
 	public String getCategoriesAsString() {
 		IWUserContext iwuc = IWContext.getInstance();
@@ -97,10 +136,10 @@ public class CategoryBean {
 		try {
 			IWSlideSession session = (IWSlideSession)IBOLookup.getSessionInstance(iwuc,IWSlideSession.class);
 			WebdavRootResource rootResource = session.getWebdavRootResource();
-//			String filePath = service.getURI(CATEORY_FILE_PATH);
+//			String filePath = service.getURI(CATEGORY_FILE_PATH);
 //			System.out.println("Loading categories for "+filePath);
 			
-			String path = getSlideService().getURI(CATEORY_CONFIG_FILE);
+			String path = getSlideService().getURI(CATEGORY_CONFIG_FILE);
 			
 			categories = rootResource.getMethodDataAsString(path);
 			if(rootResource.getStatusCode() != WebdavStatus.SC_OK){
@@ -141,84 +180,16 @@ public class CategoryBean {
 	}
 	
 	/**
-	 * <p>Stores all the given categories</p>
-	 * @param categories
-	 */
-	public void storeCategories(Collection categories) {
-		//Create a string out of the collection
-		StringBuffer sb = new StringBuffer();
-		boolean first = true;
-		Iterator iter = categories.iterator();
-		while(iter.hasNext()) {
-			if(!first) {
-				sb.append(CATEGORY_DELIMETER);
-			}
-			first=false;
-			sb.append(iter.next().toString());
-		}
-		storeCategories(sb.toString());
-	}
-
-	/**
-	 * <p>Store categories</p>
-	 * @param categories
-	 */
-	public void storeCategories(String categories) {
-		IWContext iwc = IWContext.getInstance();
-		
-		try {
-			IWSlideSession session = (IWSlideSession)IBOLookup.getSessionInstance(iwc,IWSlideSession.class);
-
-			WebdavRootResource rootResource = session.getWebdavRootResource();
-			//TODO make sure that the folder exists
-			//boolean hadToCreate = 
-			String path = getSlideService().getURI(CATEORY_CONFIG_PATH);
-			String filePath = getSlideService().getURI(CATEORY_CONFIG_FILE);
-			session.createAllFoldersInPath(path);
-//			System.out.println("Had to create folder "+hadToCreate);
-//			System.out.println("Storing categories "+categories+" to file "+CATEORY_CONFIG_FILE);
-			boolean putOK = rootResource.putMethod(CATEORY_CONFIG_FILE, categories);
-//			System.out.println("Put to "+CATEORY_CONFIG_FILE+" was "+putOK);
-			if(!putOK) {
-				putOK = rootResource.putMethod(filePath, categories);
-				System.out.println("Put to "+ filePath +" was "+putOK);
-			}
-//			IWSlideService service = (IWSlideService)IBOLookup.getServiceInstance(iwc,IWSlideService.class);
-//			String filePath = service.getURI(CATEORY_FILE_PATH);
-//			WebdavResource webdavResource = new WebdavResource(filePath);
-//			webdavResource.putMethod(categories);
-		}
-		catch (IBOLookupException e) {
-			e.printStackTrace();
-		}
-		catch (HttpException e) {
-			e.printStackTrace();
-		}
-		catch (RemoteException e) {
-			e.printStackTrace();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
 	 * <p>Adds a category to the available categories</p>
 	 * @param category
 	 */
 	public void addCategory(String category) {
-		if(getCategories().contains(category)) {
-			return;
-		}
-		StringBuffer sb = new StringBuffer(
-				getCategoriesAsString()
-				);
-		if(sb.length()>0 && !sb.toString().endsWith(CATEGORY_DELIMETER)) {
-			sb.append(CATEGORY_DELIMETER);
-		}
-		sb.append(category);
-		System.out.println("New category string to store:"+sb.toString()+"  New category:"+category);
-		storeCategories(sb.toString());
+		String key = getCategoryKey(category);
+		resourceBundle.put(key, category);
+	}
+
+	private String getCategoryKey(String category) {
+		return StringHandler.stripNonRomanCharacters(category).toLowerCase();
 	}
 
 }
