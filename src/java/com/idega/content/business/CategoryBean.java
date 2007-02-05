ddd1a25626111a76f14c308accb561dec3891089
Gediminas Paulauskas <gediminas@idega.com>
2007-02-05 21:39:26 +0000

    store categories in xml file

--- src/java/com/idega/content/business/CategoryBean.java
+++ src/java/com/idega/content/business/CategoryBean.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CategoryBean.java,v 1.7 2007/02/04 23:58:02 laddi Exp $
+ * $Id: CategoryBean.java,v 1.8 2007/02/05 21:39:26 gediminas Exp $
  *
  * Copyright (C) 2004 Idega. All Rights Reserved.
  *
@@ -9,24 +9,35 @@
  */
 package com.idega.content.business;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.StringTokenizer;
-
 import org.apache.commons.httpclient.HttpException;
 import org.apache.webdav.lib.PropertyName;
 import org.apache.webdav.lib.WebdavResource;
 import org.apache.webdav.lib.util.WebdavStatus;
-
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.filter.Filter;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
+import com.idega.content.data.ContentCategory;
 import com.idega.idegaweb.IWMainApplication;
 import com.idega.idegaweb.IWUserContext;
+import com.idega.io.MemoryFileBuffer;
+import com.idega.io.MemoryInputStream;
+import com.idega.io.MemoryOutputStream;
 import com.idega.presentation.IWContext;
 import com.idega.slide.business.IWSlideService;
 import com.idega.slide.business.IWSlideSession;
@@ -39,10 +50,10 @@ import com.idega.util.StringHandler;
  * Class for manipulating Categories that are stored in slide.<br/>
  * Includes functions for getting and setting all the available categories
  * </p>
- *  Last modified: $Date: 2007/02/04 23:58:02 $ by $Author: laddi $
+ *  Last modified: $Date: 2007/02/05 21:39:26 $ by $Author: gediminas $
  * 
  * @author <a href="mailto:Joakim@idega.com">Joakim</a>,<a href="mailto:tryggvi@idega.com">Tryggvi Larusson</a>
- * @version $Revision: 1.7 $
+ * @version $Revision: 1.8 $
  */
 public class CategoryBean {
 	
@@ -53,15 +64,15 @@ public class CategoryBean {
 	 * @deprecated this file will no longer be used
 	 */
 	private static final String CATEGORY_CONFIG_FILE = CATEGORY_CONFIG_PATH+"categories.prop";
-	private static final String CATEGORY_PROPERTIES_FILE = CATEGORY_CONFIG_PATH+"categories.strings";
+	private static final String CATEGORY_PROPERTIES_FILE = CATEGORY_CONFIG_PATH+"categories.xml";
 	private IWMainApplication iwma;
-	protected WebDAVResourceBundle resourceBundle;
+	private Map<String, ContentCategory> categories;
 	
 	public static final String CATEGORY_DELIMETER = ",";
 
 	protected CategoryBean(IWMainApplication iwma){
 		this.iwma=iwma;
-		this.resourceBundle = new WebDAVResourceBundle(CATEGORY_PROPERTIES_FILE);
+		this.categories = loadCategories();
 		if (getCategories().isEmpty()) {
 			CategoriesMigrator migrator = new CategoriesMigrator();
 			Collection oldCategories = getCategoriesFromString(getCategoriesAsString());
@@ -69,24 +80,28 @@ public class CategoryBean {
 		}
 	}
 
-	protected class CategoriesMigrator {
+	private class CategoriesMigrator {
 		private final String PROPERTY_NAME_CATEGORIES = new PropertyName("DAV","categories").toString();
 
 		private HashMap valuesToKeys;
 		private IWSlideSession session;
 		private IWSlideService service;
 		
-		protected void migrate(Collection cats) {
+		private void migrate(Collection cats) {
 			System.out.println("Migrating " + CATEGORY_CONFIG_FILE + " to new format at " + CATEGORY_PROPERTIES_FILE);
-			HashMap map = new HashMap();
+			categories = new HashMap();
 			valuesToKeys = new HashMap();
+			String lang = getCurrentLocale();
 			for (Iterator iter = cats.iterator(); iter.hasNext();) {
 				String cat = (String) iter.next();
 				String key = CategoryBean.getCategoryKey(cat);
-				map.put(key, cat);
+				ContentCategory category = new ContentCategory(key);
+				category.addName(lang, cat);
+				categories.put(key, category);
 				valuesToKeys.put(cat, key);
 			}
-			resourceBundle.putAll(map);
+			
+			storeCategories();
 			
 			try {
 				IWContext iwc = IWContext.getInstance();
@@ -178,7 +193,17 @@ public class CategoryBean {
 		return bean;
 	}
 	
-	public IWSlideService getSlideService(){
+	public IWSlideSession getSlideSession() {
+		try {
+			IWContext iwc = IWContext.getInstance();
+			IWSlideSession session = (IWSlideSession)IBOLookup.getSessionInstance(iwc,IWSlideSession.class);
+			return session;
+		} catch (IBOLookupException e) {
+			throw new RuntimeException("Error getting IWSlideSession");
+		}
+	}
+
+	public IWSlideService getSlideService() {
 		try {
 			return (IWSlideService)IBOLookup.getServiceInstance(this.iwma.getIWApplicationContext(),IWSlideService.class);
 		}
@@ -192,14 +217,17 @@ public class CategoryBean {
 	 * @return collection of strings
 	 */
 	public Collection getCategories() {
-		//Collection ret = getCategoriesFromString(getCategoriesAsString());
-		return resourceBundle.keySet();
+		return this.categories.keySet();
 	}
 	
 	public String getCategoryName(String categoryKey) {
-		return resourceBundle.getString(categoryKey);
+		return this.categories.get(categoryKey).getName(getCurrentLocale());
 	}
 	
+	private String getCurrentLocale() {
+		return IWContext.getInstance().getCurrentLocale().toString();
+	}
+
 	/**
 	 * <p>Getts all the categories as one String (comma separated)</p>
 	 * @return categories
@@ -260,12 +288,94 @@ public class CategoryBean {
 	 */
 	public void addCategory(String category) {
 		String key = getCategoryKey(category);
-		resourceBundle.put(key, category);
+		String lang = getCurrentLocale();
+		ContentCategory cat = new ContentCategory(key);
+		cat.addName(lang, category);
+		this.categories.put(key, cat);
+		storeCategories();
 	}
 
-	private static final char[] LEAVE_AS_IS = {'1','2','3','4','5','6','7','8','9','0','-'};
-	
+	protected static final char[] LEAVE_AS_IS = {'-','0','1','2','3','4','5','6','7','8','9'};
 	protected static String getCategoryKey(String category) {
 		return StringHandler.stripNonRomanCharacters(category, LEAVE_AS_IS).toLowerCase();
 	}
+	
+	protected Map loadCategories() {
+		Map map = new HashMap();
+		
+		try {
+			String resourcePath = getSlideService().getURI(CATEGORY_PROPERTIES_FILE);
+			WebdavRootResource rootResource = getSlideSession().getWebdavRootResource();
+			InputStream in = rootResource.getMethodData(resourcePath);
+
+			if (rootResource.getStatusCode() != WebdavStatus.SC_OK) {
+				System.err.println("Could not load from webdav: " + rootResource.getStatusMessage());
+				throw new FileNotFoundException("File not found " + CATEGORY_PROPERTIES_FILE);
+			}
+			
+			SAXBuilder builder = new SAXBuilder();
+			Document document = builder.build(in);
+			Element root = document.getRootElement();
+			Iterator cats = root.getDescendants(new Filter() {
+				public boolean matches(Object obj) {
+					if (obj instanceof Element) {
+						Element elem = (Element) obj;
+						if ("category".equals(elem.getName())) {
+							return true;
+						}
+					}
+					return false;
+				}
+			});
+			while (cats.hasNext()) {
+				Element cat = (Element) cats.next();
+				ContentCategory category = new ContentCategory(cat);
+				map.put(category.getId(), category);
+			}
+		} catch (IOException e) {
+			System.err.println("Error loading file " + CATEGORY_PROPERTIES_FILE + ": " + e.getMessage());
+		}
+		catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return map;
+	}
+
+	public synchronized void storeCategories() {
+		try {
+			Element root = new Element("categories");
+			Iterator keys = this.categories.keySet().iterator();
+			while (keys.hasNext()) {
+				String key = (String) keys.next();
+				ContentCategory category = this.categories.get(key);
+				Element cat = category.getAsXML();
+				root.addContent(cat);
+			}
+			Document document = new Document(root);
+			
+			MemoryFileBuffer buf = new MemoryFileBuffer();
+			MemoryOutputStream out = new MemoryOutputStream(buf);
+			XMLOutputter outputter = new XMLOutputter();
+			outputter.output(document, out);
+			out.close();
+			
+			MemoryInputStream in = new MemoryInputStream(buf);
+
+			String resourcePath = getSlideService().getURI(CATEGORY_PROPERTIES_FILE);
+			System.out.println("Writing file to " + resourcePath);
+	
+			WebdavRootResource rootResource = getSlideSession().getWebdavRootResource();
+			boolean putOK = rootResource.putMethod(resourcePath, in);
+			if (!putOK) {
+				System.err.println("Could not store to webdav: " + rootResource.getStatusMessage());
+			}
+			in.close();
+		} catch (IOException e) {
+			System.err.println("Error storing file " + CATEGORY_PROPERTIES_FILE + ": " + e.getMessage());
+		}
+	}
+
+	
 }
