0bb6f620c16fd4533f57677e723b8bc634bd97f1
Gediminas Paulauskas <gediminas@idega.com>
2007-02-06 01:12:31 +0000

    store categories in xml file

---
+++ src/java/com/idega/content/data/ContentCategory.java
@@ -0,0 +1,116 @@
+/**
+ * $Id: ContentCategory.java,v 1.1.2.2 2007/02/06 01:12:31 gediminas Exp $
+ * Created in 2007 by gediminas
+ *
+ * Copyright (C) 2000-2007 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.content.data;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.jdom.Attribute;
+import org.jdom.Element;
+import org.jdom.Namespace;
+
+
+/**
+ * Describes a category
+ * 
+ * @author <a href="mailto:gediminas@idega.com">Gediminas Paulauskas</a>
+ */
+public class ContentCategory {
+	
+	private String id;
+	private boolean disabled = false;
+	private Map names;
+	
+	private ContentCategory() {
+	}
+
+	public ContentCategory(String id) {
+		this.id = id;
+		this.names = new HashMap();
+	}
+	
+	/**
+	 * @param cat
+	 */
+	public ContentCategory(Element cat) {
+		setFromXML(cat);
+	}
+
+	public String getId() {
+		return this.id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public boolean isDisabled() {
+		return this.disabled;
+	}
+	
+	public void setDisabled(boolean disabled) {
+		this.disabled = disabled;
+	}
+	
+	public Map getNames() {
+		return this.names;
+	}
+	
+	public void setNames(Map names) {
+		this.names = names;
+	}
+	
+	public String getName(String lang) {
+		return (String) getNames().get(lang);
+	}
+	
+	public void addName(String lang, String name) {
+		getNames().put(lang, name);
+	}
+
+	public void setFromXML(Element cat) {
+		this.id = cat.getAttributeValue("id");
+		Attribute disabledAttr = cat.getAttribute("disabled");
+		if (disabledAttr != null) {
+			this.disabled = Boolean.getBoolean(disabledAttr.getValue());
+		}
+		else {
+			this.disabled = false;
+		}
+		this.names = new HashMap();
+		List namesEl = cat.getChildren("name");
+		for (Iterator iter = namesEl.iterator(); iter.hasNext(); ) {
+			Element name = (Element) iter.next();
+			String lang = name.getAttributeValue("lang");
+			String localizedName = name.getText();
+			names.put(lang, localizedName);
+		}
+
+	}
+	
+	public Element getAsXML() {
+		Element cat = new Element("category");
+		cat.setAttribute("id", getId());
+		if (isDisabled()) {
+			cat.setAttribute("disabled", String.valueOf(isDisabled()));
+		}
+		for (Iterator iter = getNames().keySet().iterator(); iter.hasNext(); ) {
+			String lang = (String) iter.next();
+			String name = (String) getNames().get(lang);
+			Element locName = new Element("name");
+			locName.setAttribute("lang", lang, Namespace.XML_NAMESPACE);
+			locName.setText(name);
+			cat.addContent(locName);
+		}
+		return cat;
+	}
+	
+}
