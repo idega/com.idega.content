aa34bc09b587eaae4966e5e7a746a24fcc95c646
Thorhallur Helgason <laddi@idega.com>
2007-05-21 07:57:41 +0000

    Declaring new block version.

--- src/java/com/idega/content/renderkit/ContentListViewerRenderer.java
+++ src/java/com/idega/content/renderkit/ContentListViewerRenderer.java
@@ -1,11 +1,9 @@
 /*
- * $Id: ContentListViewerRenderer.java,v 1.10 2006/05/11 16:09:47 eiki Exp $ Created on
- * 27.1.2005
+ * $Id: ContentListViewerRenderer.java,v 1.10.2.1 2007/05/21 07:57:42 laddi Exp $ Created on 27.1.2005
  * 
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
  * 
- * This software is the proprietary information of Idega hf. Use is subject to
- * license terms.
+ * This software is the proprietary information of Idega hf. Use is subject to license terms.
  */
 package com.idega.content.renderkit;
 
@@ -31,18 +29,17 @@ import com.idega.webface.renderkit.BaseRenderer;
 
 /**
  * 
- * Last modified: $Date: 2006/05/11 16:09:47 $ by $Author: eiki $
+ * Last modified: $Date: 2007/05/21 07:57:42 $ by $Author: laddi $
  * 
  * @author <a href="mailto:gummi@idega.com">Gudmundur Agust Saemundsson </a>
- * @version $Revision: 1.10 $
+ * @version $Revision: 1.10.2.1 $
  */
 public class ContentListViewerRenderer extends BaseRenderer {
-	
+
 	public static final String DIV_ELEM = "div";
 
 	/**
-	 * Gets the default Logger. By default it uses the package and the class
-	 * name to get the logger. <br>
+	 * Gets the default Logger. By default it uses the package and the class name to get the logger. <br>
 	 * This behaviour can be overridden in subclasses.
 	 * 
 	 * @return the default Logger
@@ -67,8 +64,8 @@ public class ContentListViewerRenderer extends BaseRenderer {
 		writer.startElement(DIV_ELEM, uiComponent);
 		writer.writeAttribute(HTML.ID_ATTR, uiComponent.getClientId(facesContext), null);
 		HtmlRendererUtils.renderHTMLAttributes(writer, uiComponent, HTML.COMMON_PASSTROUGH_ATTRIBUTES);
-//		renderFacet(facesContext, writer, (UIData) uiComponent, true);
-		renderHeader(facesContext,(UIData)uiComponent);
+// renderFacet(facesContext, writer, (UIData) uiComponent, true);
+		renderHeader(facesContext, (UIData) uiComponent);
 	}
 
 	/**
@@ -78,8 +75,9 @@ public class ContentListViewerRenderer extends BaseRenderer {
 	 */
 	public void renderHeader(FacesContext facesContext, UIData data) throws IOException {
 		UIComponent comp = data.getHeader();
-		RenderUtils.renderChild(facesContext,comp);
+		RenderUtils.renderChild(facesContext, comp);
 	}
+
 	/**
 	 * @param facesContext
 	 * @param data
@@ -87,17 +85,17 @@ public class ContentListViewerRenderer extends BaseRenderer {
 	 */
 	public void renderFooter(FacesContext facesContext, UIData data) throws IOException {
 		UIComponent comp = data.getFooter();
-		RenderUtils.renderChild(facesContext,comp);
+		RenderUtils.renderChild(facesContext, comp);
 	}
 
-	public void encodeChildren(FacesContext facesContext, UIComponent component) throws IOException {		
+	public void encodeChildren(FacesContext facesContext, UIComponent component) throws IOException {
 		RendererUtils.checkParamValidity(facesContext, component, UIData.class);
 		UIData uiData = (UIData) component;
 		ResponseWriter writer = facesContext.getResponseWriter();
-		
+
 		String savedArticleItemStyleClass = null;
 		String firstArticleItemStyleClass = null;
-		
+
 		String rowClasses;
 		String columnClasses;
 		if (component instanceof ContentItemListViewer) {
@@ -109,7 +107,7 @@ public class ContentListViewerRenderer extends BaseRenderer {
 			rowClasses = (String) component.getAttributes().get(JSFAttr.ROW_CLASSES_ATTR);
 			columnClasses = (String) component.getAttributes().get(JSFAttr.COLUMN_CLASSES_ATTR);
 		}
-		
+
 		Styles styles = new Styles(rowClasses, columnClasses);
 		int first = uiData.getFirst();
 		int rows = uiData.getRows();
@@ -122,14 +120,14 @@ public class ContentListViewerRenderer extends BaseRenderer {
 			last = rowCount;
 		}
 		for (int i = first; i < last; i++) {
-			uiData.setRowIndex(i);   
-			
+			uiData.setRowIndex(i);
+
 			if (!uiData.isRowAvailable()) {
 				logError("Row is not available. Rowindex = " + i);
 				return;
 			}
 			beforeViewer(facesContext, uiData);
-			
+
 			if (styles.hasRowStyle()) {
 				HtmlRendererUtils.writePrettyLineSeparator(facesContext);
 				writer.startElement(DIV_ELEM, component);
@@ -138,8 +136,8 @@ public class ContentListViewerRenderer extends BaseRenderer {
 			}
 			List children = component.getChildren();
 			for (int j = 0, size = component.getChildCount(); j < size; j++) {
-				UIComponent child = (UIComponent) children.get(j);				
-				
+				UIComponent child = (UIComponent) children.get(j);
+
 				if (child instanceof UIColumn && ((UIColumn) child).isRendered()) {
 					if (styles.hasColumnStyle()) {
 						HtmlRendererUtils.writePrettyLineSeparator(facesContext);
@@ -148,44 +146,49 @@ public class ContentListViewerRenderer extends BaseRenderer {
 						writer.writeAttribute(HTML.CLASS_ATTR, columnStyle, null);
 					}
 					HtmlRendererUtils.writePrettyLineSeparator(facesContext);
-					
-					//different style classes to article items are assigned;
-                    //that will help to style articles 
-                    List childrenOfAChild = child.getChildren(); // the same as getting first child...
-                    for (int k = 0; k < child.getChildCount(); k++) { // because there is only one child allways 
-                        WFContainer article = (WFContainer) childrenOfAChild.get(k);
-                        if (i == first) {
-                            savedArticleItemStyleClass = article.getStyleClass();                        
-                        }
-                        StringBuffer buf = new StringBuffer();
-                        
-                        if (!(firstArticleItemStyleClass == null)) {
-                            if (i == first) { // this is first atricle item                                
-                                buf.append(firstArticleItemStyleClass);  
-                            } else {
-                                buf.append(savedArticleItemStyleClass);
-                            }
-                        } else {
-                            buf.append(savedArticleItemStyleClass);
-                        }
-                        
-                        if (i == first) {                            
-                            buf.append(" ").append(savedArticleItemStyleClass).append("_first");
-                        }
-                        if (i == last - 1) { //last                            
-                            buf.append(" ").append(savedArticleItemStyleClass).append("_last");
-                        }
-                        
-                        if ((i % 2) == 0) {
-                            buf.append(" ").append(savedArticleItemStyleClass).append("_odd");
-                        } else {
-                            buf.append(" ").append(savedArticleItemStyleClass).append("_even");
-                        }
-                        
-                        article.setStyleClass(buf.toString());
-                        
-                    }
-					
+
+					// different style classes to article items are assigned;
+					// that will help to style articles
+					List childrenOfAChild = child.getChildren(); // the same as getting first child...
+					for (int k = 0; k < child.getChildCount(); k++) { // because there is only one child allways
+						WFContainer article = (WFContainer) childrenOfAChild.get(k);
+						if (i == first) {
+							savedArticleItemStyleClass = article.getStyleClass();
+						}
+						StringBuffer buf = new StringBuffer();
+
+						if (!(firstArticleItemStyleClass == null)) {
+							if (i == first) { // this is first atricle item
+								buf.append(firstArticleItemStyleClass);
+							}
+							else {
+								buf.append(savedArticleItemStyleClass);
+							}
+						}
+						else {
+							buf.append(savedArticleItemStyleClass);
+						}
+
+						if (i == first) {
+							buf.append(" ").append(savedArticleItemStyleClass).append("_first");
+						}
+						if (i == last - 1) { // last
+							buf.append(" ").append(savedArticleItemStyleClass).append("_last");
+						}
+
+						if ((i % 2) == 0) {
+							buf.append(" ").append(savedArticleItemStyleClass).append("_odd");
+						}
+						else {
+							buf.append(" ").append(savedArticleItemStyleClass).append("_even");
+						}
+
+						buf.append(" ").append(savedArticleItemStyleClass).append("_").append(i + 1);
+
+						article.setStyleClass(buf.toString());
+
+					}
+
 					RendererUtils.renderChild(facesContext, child);
 					if (styles.hasColumnStyle()) {
 						writer.endElement(DIV_ELEM);
@@ -226,115 +229,105 @@ public class ContentListViewerRenderer extends BaseRenderer {
 	public void encodeEnd(FacesContext facesContext, UIComponent uiComponent) throws IOException {
 		RendererUtils.checkParamValidity(facesContext, uiComponent, UIData.class);
 		ResponseWriter writer = facesContext.getResponseWriter();
-		renderFooter(facesContext,(UIData)uiComponent);
-//		renderFacet(facesContext, writer, (UIData) uiComponent, false);
+		renderFooter(facesContext, (UIData) uiComponent);
+// renderFacet(facesContext, writer, (UIData) uiComponent, false);
 		HtmlRendererUtils.writePrettyLineSeparator(facesContext);
 		writer.endElement(DIV_ELEM);
 		HtmlRendererUtils.writePrettyLineSeparator(facesContext);
 		afterViewerList(facesContext, (UIData) uiComponent);
 	}
 
-	protected void renderViewerListHeaderRow(FacesContext facesContext, ResponseWriter writer, UIData uiData,
-			UIComponent headerFacet, String headerStyleClass) throws IOException {
+	protected void renderViewerListHeaderRow(FacesContext facesContext, ResponseWriter writer, UIData uiData, UIComponent headerFacet, String headerStyleClass) throws IOException {
 		renderViewerListHeaderOrFooterRow(facesContext, writer, uiData, headerFacet, headerStyleClass, DIV_ELEM);
 	}
 
-	protected void renderViewerListFooterRow(FacesContext facesContext, ResponseWriter writer, UIData uiData,
-			UIComponent footerFacet, String footerStyleClass) throws IOException {
+	protected void renderViewerListFooterRow(FacesContext facesContext, ResponseWriter writer, UIData uiData, UIComponent footerFacet, String footerStyleClass) throws IOException {
 		renderViewerListHeaderOrFooterRow(facesContext, writer, uiData, footerFacet, footerStyleClass, DIV_ELEM);
 	}
 
-	protected void renderColumnHeaderRow(FacesContext facesContext, ResponseWriter writer, UIData uiData,
-			String headerStyleClass) throws IOException {
+	protected void renderColumnHeaderRow(FacesContext facesContext, ResponseWriter writer, UIData uiData, String headerStyleClass) throws IOException {
 		renderColumnHeaderOrFooterRow(facesContext, writer, uiData, headerStyleClass, true);
 	}
 
-	protected void renderColumnFooterRow(FacesContext facesContext, ResponseWriter writer, UIData uiData,
-			String footerStyleClass) throws IOException {
+	protected void renderColumnFooterRow(FacesContext facesContext, ResponseWriter writer, UIData uiData, String footerStyleClass) throws IOException {
 		renderColumnHeaderOrFooterRow(facesContext, writer, uiData, footerStyleClass, false);
 	}
 
-	private void renderViewerListHeaderOrFooterRow(FacesContext facesContext, ResponseWriter writer, UIData uiData,
-			UIComponent facet, String styleClass, String colElementName) throws IOException {
-//		HtmlRendererUtils.writePrettyLineSeparator(facesContext);
-//		writer.startElement(HTML.TR_ELEM, uiData);
-//		writer.startElement(colElementName, uiData);
-//		if (colElementName.equals(HTML.TH_ELEM)) {
-//			writer.writeAttribute(HTML.SCOPE_ATTR, HTML.SCOPE_COLGROUP_VALUE, null);
-//		}
-//		writer.writeAttribute(HTML.COLSPAN_ATTR, new Integer(colspan), null);
-//		if (styleClass != null) {
-//			writer.writeAttribute(HTML.CLASS_ATTR, styleClass, null);
-//		}
-//		if (facet != null) {
-//			RendererUtils.renderChild(facesContext, facet);
-//		}
-//		writer.endElement(colElementName);
-//		writer.endElement(HTML.TR_ELEM);
+	private void renderViewerListHeaderOrFooterRow(FacesContext facesContext, ResponseWriter writer, UIData uiData, UIComponent facet, String styleClass, String colElementName) throws IOException {
+// HtmlRendererUtils.writePrettyLineSeparator(facesContext);
+// writer.startElement(HTML.TR_ELEM, uiData);
+// writer.startElement(colElementName, uiData);
+// if (colElementName.equals(HTML.TH_ELEM)) {
+// writer.writeAttribute(HTML.SCOPE_ATTR, HTML.SCOPE_COLGROUP_VALUE, null);
+// }
+// writer.writeAttribute(HTML.COLSPAN_ATTR, new Integer(colspan), null);
+// if (styleClass != null) {
+// writer.writeAttribute(HTML.CLASS_ATTR, styleClass, null);
+// }
+// if (facet != null) {
+// RendererUtils.renderChild(facesContext, facet);
+// }
+// writer.endElement(colElementName);
+// writer.endElement(HTML.TR_ELEM);
 	}
 
-	private void renderColumnHeaderOrFooterRow(FacesContext facesContext, ResponseWriter writer, UIData uiData,
-			String styleClass, boolean header) throws IOException {
-//		HtmlRendererUtils.writePrettyLineSeparator(facesContext);
-//		writer.startElement(HTML.TR_ELEM, uiData);
-//		for (Iterator it = uiData.getChildren().iterator(); it.hasNext();) {
-//			UIComponent uiComponent = (UIComponent) it.next();
-//			if (uiComponent instanceof UIColumn && ((UIColumn) uiComponent).isRendered()) {
-//				if (header) {
-//					renderColumnHeaderCell(facesContext, writer, (UIColumn) uiComponent, styleClass);
-//				}
-//				else {
-//					renderColumnFooterCell(facesContext, writer, (UIColumn) uiComponent, styleClass);
-//				}
-//			}
-//		}
-//		writer.endElement(HTML.TR_ELEM);
+	private void renderColumnHeaderOrFooterRow(FacesContext facesContext, ResponseWriter writer, UIData uiData, String styleClass, boolean header) throws IOException {
+// HtmlRendererUtils.writePrettyLineSeparator(facesContext);
+// writer.startElement(HTML.TR_ELEM, uiData);
+// for (Iterator it = uiData.getChildren().iterator(); it.hasNext();) {
+// UIComponent uiComponent = (UIComponent) it.next();
+// if (uiComponent instanceof UIColumn && ((UIColumn) uiComponent).isRendered()) {
+// if (header) {
+// renderColumnHeaderCell(facesContext, writer, (UIColumn) uiComponent, styleClass);
+// }
+// else {
+// renderColumnFooterCell(facesContext, writer, (UIColumn) uiComponent, styleClass);
+// }
+// }
+// }
+// writer.endElement(HTML.TR_ELEM);
 	}
 
-	protected void renderColumnHeaderCell(FacesContext facesContext, ResponseWriter writer, UIColumn uiColumn,
-			String headerStyleClass) throws IOException {
-//		writer.startElement(HTML.TH_ELEM, uiColumn);
-//		if (headerStyleClass != null) {
-//			writer.writeAttribute(HTML.CLASS_ATTR, headerStyleClass, null);
-//		}
-//		UIComponent facet = uiColumn.getHeader();
-//		if (facet != null) {
-//			RendererUtils.renderChild(facesContext, facet);
-//		}
-//		writer.endElement(HTML.TH_ELEM);
+	protected void renderColumnHeaderCell(FacesContext facesContext, ResponseWriter writer, UIColumn uiColumn, String headerStyleClass) throws IOException {
+// writer.startElement(HTML.TH_ELEM, uiColumn);
+// if (headerStyleClass != null) {
+// writer.writeAttribute(HTML.CLASS_ATTR, headerStyleClass, null);
+// }
+// UIComponent facet = uiColumn.getHeader();
+// if (facet != null) {
+// RendererUtils.renderChild(facesContext, facet);
+// }
+// writer.endElement(HTML.TH_ELEM);
 	}
 
-	protected void renderColumnFooterCell(FacesContext facesContext, ResponseWriter writer, UIColumn uiColumn,
-			String footerStyleClass) throws IOException {
-//		writer.startElement(HTML.TD_ELEM, uiColumn);
-//		if (footerStyleClass != null) {
-//			writer.writeAttribute(HTML.CLASS_ATTR, footerStyleClass, null);
-//		}
-//		UIComponent facet = uiColumn.getFooter();
-//		if (facet != null) {
-//			RendererUtils.renderChild(facesContext, facet);
-//		}
-//		writer.endElement(HTML.TD_ELEM);
+	protected void renderColumnFooterCell(FacesContext facesContext, ResponseWriter writer, UIColumn uiColumn, String footerStyleClass) throws IOException {
+// writer.startElement(HTML.TD_ELEM, uiColumn);
+// if (footerStyleClass != null) {
+// writer.writeAttribute(HTML.CLASS_ATTR, footerStyleClass, null);
+// }
+// UIComponent facet = uiColumn.getFooter();
+// if (facet != null) {
+// RendererUtils.renderChild(facesContext, facet);
+// }
+// writer.endElement(HTML.TD_ELEM);
 	}
 
-	//-------------------------------------------------------------
+	// -------------------------------------------------------------
 	// Helper class Styles
-	//-------------------------------------------------------------
+	// -------------------------------------------------------------
 	private static class Styles {
 
-		//~ Instance fields
+		// ~ Instance fields
 		// ------------------------------------------------------------------------
 		private String[] _columnStyle;
 
 		private String[] _rowStyle;
 
-		//~ Constructors
+		// ~ Constructors
 		// ---------------------------------------------------------------------------
 		Styles(String rowStyles, String columnStyles) {
-			this._rowStyle = (rowStyles == null) ? ArrayUtils.EMPTY_STRING_ARRAY
-					: StringUtils.trim(StringUtils.splitShortString(rowStyles, ','));
-			this._columnStyle = (columnStyles == null) ? ArrayUtils.EMPTY_STRING_ARRAY
-					: StringUtils.trim(StringUtils.splitShortString(columnStyles, ','));
+			this._rowStyle = (rowStyles == null) ? ArrayUtils.EMPTY_STRING_ARRAY : StringUtils.trim(StringUtils.splitShortString(rowStyles, ','));
+			this._columnStyle = (columnStyles == null) ? ArrayUtils.EMPTY_STRING_ARRAY : StringUtils.trim(StringUtils.splitShortString(columnStyles, ','));
 		}
 
 		public String getRowStyle(int idx) {
