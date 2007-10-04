5373fff2f37a63af1af0aba17175e9cbe97e38ec
Valdas Zemaitis <valdas@idega.com>
2007-10-04 13:32:17 +0000

    Added generic type

--- src/java/com/idega/content/bean/ContentItemBeanComparator.java
+++ src/java/com/idega/content/bean/ContentItemBeanComparator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ContentItemBeanComparator.java,v 1.4 2007/02/20 13:53:42 valdas Exp $
+ * $Id: ContentItemBeanComparator.java,v 1.5 2007/10/04 13:32:17 valdas Exp $
  * Created on 15.2.2005
  *
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -15,12 +15,12 @@ import java.util.Date;
 
 /**
  * 
- *  Last modified: $Date: 2007/02/20 13:53:42 $ by $Author: valdas $
+ *  Last modified: $Date: 2007/10/04 13:32:17 $ by $Author: valdas $
  * 
  * @author <a href="mailto:gummi@idega.com">Gudmundur Agust Saemundsson</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
-public class ContentItemBeanComparator implements Comparator {
+public class ContentItemBeanComparator implements Comparator<ContentItemBean> {
 
 	private boolean reverse = false;
 	
@@ -38,24 +38,22 @@ public class ContentItemBeanComparator implements Comparator {
 	/* (non-Javadoc)
 	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
 	 */
-	public int compare(Object o1, Object o2) {
-		ContentItemBean item1 = (ContentItemBean)o1;
-		ContentItemBean item2 = (ContentItemBean)o2;
+	public int compare(ContentItemBean item1, ContentItemBean item2) {
 		int returner = 0;
 		
 		Date item1Date = item1.getPublishedDate() == null ? item1.getCreationDate() : item1.getPublishedDate();
 		Date item2Date = item2.getPublishedDate() == null ? item2.getCreationDate() : item2.getPublishedDate();
 		
-		if(item1Date==null && item2Date==null){
+		if (item1Date == null && item2Date == null) {
 			returner = 0;
 		}
-		else if(item2Date==null){
+		else if (item2Date == null) {
 			returner = 1;
 		}
-		else if(item1Date==null){
+		else if (item1Date == null) {
 			returner = -1;
 		}
-		else{
+		else {
 			returner = item1Date.compareTo(item2Date);
 		}
 		return returner*((this.reverse)?-1:1);
