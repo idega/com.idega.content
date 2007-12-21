05a79260cc39477bcb886d2a36b5911cc88c36f2
Valdas Zemaitis <valdas@idega.com>
2007-12-21 19:53:46 +0000

    Removed needless ones

--- scripts/NodeID.js
+++
@@ -1,7 +0,0 @@
-function NodeID() { }
-
-NodeID._path = '/dwr';
-
-NodeID.OldIDsToJavaScript = function(p0,callback) {
-    DWREngine._execute(NodeID._path, 'NodeID', 'OldIDsToJavaScript', callback);
-}
