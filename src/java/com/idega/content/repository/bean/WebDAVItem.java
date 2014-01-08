e544ce4025efbc01f35131d34d34389a1cc8471b
Valdas Žemaitis <valdas@idega.com>
2014-01-08 10:25:37 +0200

    Implementing 2 new methods

--- src/java/com/idega/content/repository/bean/WebDAVItem.java
+++ src/java/com/idega/content/repository/bean/WebDAVItem.java
@@ -2,8 +2,12 @@ package com.idega.content.repository.bean;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 import org.apache.webdav.lib.WebdavResource;
+import org.apache.webdav.lib.WebdavResources;
 
 import com.idega.repository.bean.RepositoryItem;
 import com.idega.util.CoreConstants;
@@ -13,16 +17,16 @@ public class WebDAVItem implements RepositoryItem {
 
 	private String name, folder;
 	private WebdavResource resource;
-	
+
 	public WebDAVItem(WebdavResource resource) {
 		this(resource, null);
 	}
-	
+
 	public WebDAVItem(WebdavResource resource, String folder) {
 		this.resource = resource;
 		this.folder = folder;
 	}
-		
+
 	@Override
 	public InputStream getInputStream() throws IOException {
 		return resource.getMethodData();
@@ -35,7 +39,7 @@ public class WebDAVItem implements RepositoryItem {
 			if (name == null) {
 				name = "unknown";
 			}
-			
+
 			if (name.endsWith(CoreConstants.SLASH)) {
 				name = name.substring(0, name.lastIndexOf(CoreConstants.SLASH));
 			}
@@ -45,10 +49,10 @@ public class WebDAVItem implements RepositoryItem {
 			if (!StringUtil.isEmpty(folder) && name.startsWith(folder)) {
 				name = name.replace(folder, CoreConstants.EMPTY);
 			}
-			
+
 			this.name = name;
 		}
-		
+
 		return this.name;
 	}
 
@@ -65,5 +69,41 @@ public class WebDAVItem implements RepositoryItem {
 	@Override
 	public String toString() {
 		return getClass().getSimpleName().concat(": ").concat(getName());
+	}
+
+	@Override
+	public boolean isDirectory() {
+		return resource.getIsCollection();
+	}
+
+	private List<WebDAVItem> children = null;
+
+	@Override
+	public List<WebDAVItem> getChildren() {
+		if (children != null) {
+			return children;
+		}
+
+		children = new ArrayList<WebDAVItem>();
+
+		try {
+			WebdavResources resources = resource.getChildResources();
+			if (resources != null) {
+				@SuppressWarnings("unchecked")
+				Enumeration<WebdavResource> webDAVChildren = resources.getResources();
+				if (webDAVChildren != null) {
+					while (webDAVChildren.hasMoreElements()) {
+						WebdavResource child = webDAVChildren.nextElement();
+						if (child != null) {
+							children.add(new WebDAVItem(child));
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return children;
 	}
 }
