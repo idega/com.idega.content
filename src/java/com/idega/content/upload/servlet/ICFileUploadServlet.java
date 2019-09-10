b02b44da32ae560c1c183e5d9fa87fde49e8f40a
Valdas Žemaitis <valdas@idega.com>
2019-09-10 16:17:08 +0300

    Made possible to save file(s) in CMS

--- src/java/com/idega/content/upload/servlet/ICFileUploadServlet.java
+++ src/java/com/idega/content/upload/servlet/ICFileUploadServlet.java
@@ -1,11 +1,14 @@
 package com.idega.content.upload.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,24 +20,42 @@ import org.apache.commons.fileupload.FileUploadBase.FileSizeLimitExceededExcepti
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.google.gson.Gson;
 import com.idega.core.file.data.ICFile;
 import com.idega.core.file.data.ICFileHome;
+import com.idega.core.file.util.MimeTypeUtil;
 import com.idega.data.IDOLookup;
+import com.idega.idegaweb.IWMainApplication;
 import com.idega.presentation.IWContext;
+import com.idega.repository.RepositoryService;
+import com.idega.util.CoreConstants;
+import com.idega.util.IOUtil;
 import com.idega.util.IWTimestamp;
+import com.idega.util.StringHandler;
+import com.idega.util.expression.ELUtil;
 
-public class ICFileUploadServlet  extends HttpServlet{
+public class ICFileUploadServlet extends HttpServlet {
+
 	private static final long serialVersionUID = -1438789624273727352L;
-	
+
+	private static final Logger LOGGER = Logger.getLogger(ICFileUploadServlet.class.getName());
+
 	private Long getMaxFileSize(IWContext iwc){
 		return new Long(1024 * 1024) * 200;
 	}
-	
-	private String getServletPath(){
-		return "/servlet/ic-file-upload";
+
+	@Autowired
+	private RepositoryService repositoryService;
+
+	private RepositoryService getRepositoryService() {
+		if (repositoryService == null) {
+			ELUtil.getInstance().autowire(this);
+		}
+		return repositoryService;
 	}
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		IWContext iwc = new IWContext(request, response, getServletContext());
@@ -67,33 +88,66 @@ public class ICFileUploadServlet  extends HttpServlet{
 			}
 			responseMapArray = new ArrayList<Map<String, Object>>();
 			ICFileHome icFileHome = (ICFileHome) IDOLookup.getHome(ICFile.class);
+			InputStream stream = null;
+			boolean saveInDB = IWMainApplication.getDefaultIWMainApplication().getSettings().getBoolean("ic_file_uploader.save_in_db", true);
+			char[] exceptions = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-', '_'};
+			IWTimestamp now = IWTimestamp.RightNow();
+			String basePath = CoreConstants.PUBLIC_PATH.concat("/uploads/")
+								.concat(String.valueOf(now.getYear())).concat(CoreConstants.SLASH)
+								.concat(String.valueOf(now.getMonth())).concat(CoreConstants.SLASH)
+								.concat(String.valueOf(now.getDay())).concat(CoreConstants.SLASH)
+								.concat(String.valueOf(now.getHour())).concat(CoreConstants.MINUS).concat(String.valueOf(now.getMinute())).concat(CoreConstants.SLASH);
 			for (FileItem file: files) {
 				String fileName = file.getName();
 				ICFile icFile = icFileHome.create();
-				icFile.setFileValue(file.getInputStream());
+				stream = file.getInputStream();
+
+				boolean result = false;
+				if (!saveInDB) {
+					fileName = StringHandler.stripNonRomanCharacters(fileName, exceptions);
+					String mimeType = MimeTypeUtil.resolveMimeTypeFromFileName(fileName);
+					result = false;
+					try {
+						result = getRepositoryService().uploadFile(basePath, fileName, mimeType, stream);
+					} catch (Exception e) {
+						result = false;
+					} finally {
+						IOUtil.close(stream);
+					}
+					if (result) {
+						icFile.setFileUri(new StringBuffer(CoreConstants.WEBDAV_SERVLET_URI).append(basePath).append(fileName).toString());
+					}
+				}
+				if (!result || saveInDB) {
+					icFile.setFileValue(stream);
+				}
+
 				icFile.setName(fileName);
 				icFile.setModificationDate(IWTimestamp.getTimestampRightNow());
 				icFile.setFileSize(Long.valueOf(file.getSize()).intValue());
 				icFile.store();
 				Map<String, Object> fileData = getFileResponce(icFile);
 				responseMapArray.add(fileData);
+
+				IOUtil.close(stream);
 			}
 			Gson gson = new Gson();
 			String jsonString =  gson.toJson(responseMapArray);
 			responseWriter.write(jsonString);
 			return;
-		} catch(FileSizeLimitExceededException e){
-			log("File is too large",e);
+		} catch (FileSizeLimitExceededException e) {
+			LOGGER.log(Level.WARNING, "File is too large", e);
 			response.sendError(413);
 		}
 		catch (FileUploadException ex) {
-			log("Error encountered while parsing the request",ex);
+			LOGGER.log(Level.WARNING, "Error encountered while parsing the request",ex);
 			response.sendError(500);
-		} catch (Exception ex) {
-			log("Error encountered while uploading file",ex);
+		} catch (Throwable ex) {
+			LOGGER.log(Level.WARNING, "Error encountered while uploading file",ex);
 			response.sendError(500);
 		}
 	}
+
 	public static Map<String, Object> getFileResponce(ICFile file){
 		Map<String, Object> fileData = new HashMap<String, Object>();
 		fileData.put("name", file.getName());
